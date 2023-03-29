a0d1949c835131b1f7b33cfcba5f99b4e75610c6
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2023-03-29 20:23:04 -0300

    agregando logica para sumar dos nros y mostrarlos en una consola7

---
+++ Clase-1/Clase1/Clase1.Consola/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Clase1.Logica;
+
+Console.WriteLine("Ingrese 2 numeros: ");
+Console.WriteLine("nro 1: ");
+string nro1 = Console.ReadLine();
+
+Console.WriteLine("nro 2: ");
+string nro2 = Console.ReadLine();
+
+//string interpolation $"texto {variable}"
+Console.WriteLine($"La suma de estos nros es: {Calculadora.Sumar(nro1, nro2)}");
---
+++ Clase-1/Clase1/Clase1.Logica/Calculadora.cs
@@ -0,0 +1,14 @@
+﻿namespace Clase1.Logica;
+
+public class Calculadora
+{
+    public static int Sumar(int nro1, int nro2)
+    {
+        return nro1 + nro2;
+    }
+
+    public static int Sumar(string val1, string val2)
+    {
+        return Sumar(int.Parse(val1), int.Parse(val2));
+    }
+}
