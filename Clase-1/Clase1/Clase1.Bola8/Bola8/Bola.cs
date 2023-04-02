e2d148b01d465565dd5608d08eecd7f45d3928ba
Meistt <aguslive7@gmail.com>
2023-04-02 04:28:42 -0300

    bola 8 terminada

---
+++ Clase-1/Clase1/Clase1.Bola8/Bola8/Bola.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bola8;
+
+public class Bola
+{
+    public Respuesta respuestas = new Respuesta();
+
+    public void WelcomeMsg()
+    {
+        Console.WriteLine("¡Hola, bienvenido!");
+        Console.WriteLine("Pregunte lo que desee: ");
+        Console.WriteLine("Si desea salir del programa presione la tecla 'X'");
+    }
+
+    public string AskMe()
+    {
+        string question = Console.ReadLine();
+        Boolean isNull = IsNull(question);
+
+        //SALE POR FALSO
+        while (isNull)
+        {
+            Console.WriteLine("No ha preguntado nada aún, intente nuevamente.");
+            question = Console.ReadLine();
+            if (!IsNull(question)) { isNull = false;  }
+        }
+        return question;
+    }
+    private bool IsNull(string question)
+    {
+        if (question == "") { return true; }
+        return false;
+    }
+
+    public string GetRandomResponse(string question)
+    {
+        string response; 
+        Random r = new Random();
+        int random = r.Next(1, 3);
+        switch (random)
+        {
+            case 1:
+                return RandomAffirmativeResponse();
+                break;
+            case 2:
+                return RandomNegativeResponse();
+                break;
+            case 3:
+                return  RandomConfuseResponse();
+                break;
+        }
+        return null;
+    }
+
+    private string RandomAffirmativeResponse()
+    {
+        List<string> col = this.respuestas.NewAffirmativeResponse();
+        Random r = new Random();
+        int random = r.Next(1, col.Count);
+        return this.respuestas.AffirmativeResponse[random];
+    }
+
+    private string RandomNegativeResponse()
+    {
+        List<string> col = this.respuestas.NewNegativeResponse();
+        this.respuestas.NewNegativeResponse();
+        Random r = new Random();
+        int random = r.Next(1, col.Count);
+        return this.respuestas.NegativeResponse[random];
+    }
+
+    private string RandomConfuseResponse()
+    {
+        List<string> col = this.respuestas.NewConfuseResponse();
+        this.respuestas.NewConfuseResponse();
+        Random r = new Random();
+        int random = r.Next(1, col.Count);
+        return this.respuestas.ConfuseResponse[random];
+    }
+
+    public void CloseGame(string c)
+    {
+        if (c.Equals('x'))
+        {
+            Console.Beep();
+        }
+    }
+}
---
+++ Clase-1/Clase1/Clase1.Bola8/Bola8/Respuesta.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bola8;
+
+public class Respuesta
+{
+    public List<string> AffirmativeResponse;
+    public List<string> ConfuseResponse;
+    public List<string> NegativeResponse;
+
+    public List<string> NewAffirmativeResponse()
+    {
+        return this.AffirmativeResponse = new List<string>
+        {
+            "Es cierto.",
+            "Es decididamente así.",
+            "Sí, definitivamente.",
+            "Puedes confiar en ello.",
+            "Como yo lo veo, sí.",
+            "Lo más probable.",
+            "Perspectiva buena.",
+            "Sí.",
+            "Las señales apuntan a que sí.",
+        };
+    }
+
+    public List<string> NewConfuseResponse()
+    {
+        return this.ConfuseResponse = new List<string>
+        {
+            "Respuesta confusa, vuelve a intentarlo.",
+            "Vuelve a preguntar más tarde.",
+            "Mejor no decirte ahora.",
+            "No se puede predecir ahora.",
+            "Concéntrate y vuelve a preguntar.",
+        };
+    }
+
+    public List<string> NewNegativeResponse()
+    {
+        return this.NegativeResponse = new List<string>
+        {
+            "No cuentes con ello.",
+            "Mi respuesta es no.",
+            "Mis fuentes dicen que no.",
+            "Las perspectivas no son muy buenas.",
+            "Muy dudoso.",
+        };
+    }
+}
