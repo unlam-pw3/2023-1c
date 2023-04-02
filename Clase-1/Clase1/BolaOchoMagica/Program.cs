037acad80c53dd85aa9274a5984d55b89f6fb21a
Martin Quiroz <martinquiroz_18@hotmail.com>
2023-04-01 23:49:30 -0300

    primeros cambios

---
+++ Clase-1/Clase1/BolaOchoMagica/Program.cs
@@ -0,0 +1,46 @@
+﻿/*** Bola ocho mágica ***/
+
+Console.WriteLine("Has una pregunta: ");
+
+string pregunta = Console.ReadLine();
+
+Random rnd = new Random();
+int respuestaRandom = rnd.Next(1, 10);
+
+string respuesta = "";
+
+switch (respuestaRandom)
+{
+    case 1:
+        respuesta = "Sin lugar a dudas.";
+        break;
+    case 2:
+        respuesta = "Sí, definitivamente.";
+        break;
+    case 3:
+        respuesta = "Como yo lo veo, sí.";
+        break;
+    case 4:
+        respuesta = "Lo más probable.";
+        break;
+    case 5:
+        respuesta = "Sí.";
+        break;
+    case 6:
+        respuesta = "Mi respuesta es no.";
+        break;
+    case 7:
+        respuesta = "Mis fuentes dicen que no.";
+        break;
+    case 8:
+        respuesta = "Muy dudoso.";
+        break;
+    case 9:
+        respuesta = "No cuentes con ello.";
+        break;
+    case 10:
+        respuesta = "Muy dudoso.";
+        break;
+}
+
+Console.WriteLine(respuesta);
