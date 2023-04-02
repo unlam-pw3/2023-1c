f459f942aad931f35c76162ae7784e2f3c373992
Aibar Hernan Ezequiel <aibarhernanezequiel@gmail.com>
2023-04-02 20:08:33 +0200

    agrega ejercicio MagicBall

---
+++ Clase-1/Clase1/ConsoleApp2/Program.cs
@@ -0,0 +1,5 @@
+﻿using ConsoleApp2;
+
+Console.WriteLine("Ingrese una pregunta: ");
+string question = Console.ReadLine();
+Console.WriteLine(MagicBall.GetResponse(question));
---
+++ Clase-1/Clase1/ConsoleApp2/Response.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2;
+
+internal class Response
+{
+    public static List<string> PopulateResponsesForDefualt() 
+    {
+            StreamReader sr = new StreamReader("preguntas.txt");
+            string line = sr.ReadLine();
+            List<string> lines = new List<string>();
+
+            while (line != null)
+            {
+                lines.Add(line);
+                line = sr.ReadLine();
+            }
+
+            sr.Close();
+            return lines;
+        
+    }
+}
