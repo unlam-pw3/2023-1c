150727151794813299c4b8a5f2945cf202d2733f
nherreradev <nherrera3276@gmail.com>
2023-04-03 20:13:17 -0300

    TP1 | BOLA 8 OK

--- Clase-1/Clase1/Clase1.Consola/Program.cs
+++ Clase-1/Clase1/Clase1.Consola/Program.cs
@@ -1,11 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Clase1.Logica;
 
-using Clase1.Logica;
-using OpenAI.GPT3.Managers;
-using OpenAI.GPT3;
-using OpenAI.GPT3.ObjectModels;
-using OpenAI.GPT3.ObjectModels.RequestModels;
-using Clase1.Logica.Ejercicios;
+Bola bola = new Bola();
 
-//CalculadoraEjercicio.Ejecutar();
-await ChatGPTEjercicio.Ejecutar();
+bola.Agitar();
---
+++ Clase-1/Clase1/Clase1.Logica/Bola.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+
+namespace Clase1.Logica;
+
+public class Bola{
+
+    ArrayList answers = new ArrayList();
+
+    public Bola() {
+        fillAnswers();
+    }
+
+    private void fillAnswers() {
+             
+        answers.Add("Es cierto.");
+        answers.Add("Es decididamente así.");
+        answers.Add("Sin lugar a dudas.");
+        answers.Add("Sí, definitivamente.");
+        answers.Add("Puedes confiar en ello.");
+
+        answers.Add("Como yo lo veo, sí.");
+        answers.Add("Lo más probable.");
+        answers.Add("Perspectiva buena.");
+        answers.Add("Las señales apuntan a que sí.");
+        
+        answers.Add("Respuesta confusa, vuelve a intentarlo.");
+        answers.Add("Vuelve a preguntar más tarde.");
+        answers.Add("Mejor no decirte ahora.");
+        answers.Add("No se puede predecir ahora.");
+        answers.Add("Concéntrate y vuelve a preguntar.");
+        
+        answers.Add("No cuentes con ello.");
+        answers.Add("Mi respuesta es no.");
+        answers.Add("Mis fuentes dicen que no.");
+        answers.Add("Las perspectivas no son muy buenas.");
+        answers.Add("Muy dudoso.");
+    }
+
+
+    public void Agitar()
+    {
+       String resp = (String)answers[getRandomNumber()];
+        Console.WriteLine(resp);
+    }
+
+    private int getRandomNumber(){
+        Random r = new Random();
+        int rInt = r.Next(0, answers.Count);
+        return rInt;
+    }
+}
