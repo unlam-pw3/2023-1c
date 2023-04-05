b20084e8d779ab0a4b975baa03fd2337e7c9926e
Nicolas Kuhn <NKuhn@teco.com.ar>
2023-04-05 00:30:48 -0300

    resuelto

--- Clase-1/Clase1/Clase1.Consola/Program.cs
+++ Clase-1/Clase1/Clase1.Consola/Program.cs
@@ -1,11 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using Clase1.Logica;
-using OpenAI.GPT3.Managers;
-using OpenAI.GPT3;
-using OpenAI.GPT3.ObjectModels;
-using OpenAI.GPT3.ObjectModels.RequestModels;
+
 using Clase1.Logica.Ejercicios;
 
 //CalculadoraEjercicio.Ejecutar();
-await ChatGPTEjercicio.Ejecutar();
+//await ChatGPTEjercicio.Ejecutar();
+
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Bienvenido a la Bola del 8. Haceme una pregunta.");
+        string? pregunta = Console.ReadLine();
+
+        string respuesta = new BolaOcho().ContestarPregunta(pregunta);
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Thread.Sleep(500);
+
+            Console.WriteLine($"{new string('.', i)} \n");
+        }
+
+        Console.WriteLine($"Mi respuesta es: {respuesta}");
+
+        Console.WriteLine("¿Queres seguir preguntando?");
+
+        bool quiereSeguir = Console.ReadLine().ToLower().Equals("si");
+
+        if (!quiereSeguir) break;
+    }
+    catch
+    {
+        Console.WriteLine("Ups. No pude contestar tu pregunta. Volve a preguntar \n");
+
+    }
+}
