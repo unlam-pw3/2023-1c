b89afca48683a0ab4cd4eb72bcede2e8854326a2
Nicolas Kuhn <NKuhn@teco.com.ar>
2023-04-05 00:53:28 -0300

    feat: refactory program class

--- Clase-1/Clase1/Clase1.Consola/Program.cs
+++ Clase-1/Clase1/Clase1.Consola/Program.cs
@@ -6,33 +6,59 @@ using Clase1.Logica.Ejercicios;
 //CalculadoraEjercicio.Ejecutar();
 //await ChatGPTEjercicio.Ejecutar();
 
-while (true)
+
+public class Program
 {
-    try
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            try
+            {
+                string? pregunta = GetPregunta();
+
+                GenerarRespuesta(pregunta);
+
+                if (!QuiereSeguir()) break;
+            }
+            catch
+            {
+                Console.WriteLine("Ups. No pude contestar tu pregunta. Volve a preguntar \n");
+            }
+        }
+
+    }
+
+    private static string? GetPregunta()
     {
         Console.WriteLine("Bienvenido a la Bola del 8. Haceme una pregunta.");
-        string? pregunta = Console.ReadLine();
+        return Console.ReadLine();
+    }
 
+
+
+    private static void GenerarRespuesta(string? pregunta)
+    {
         string respuesta = new BolaOcho().ContestarPregunta(pregunta);
 
+        GenerarEspera();
+
+        Console.WriteLine($"Mi respuesta es: {respuesta}");
+    }
+
+    private static void GenerarEspera()
+    {
         for (int i = 1; i <= 5; i++)
         {
             Thread.Sleep(500);
 
             Console.WriteLine($"{new string('.', i)} \n");
         }
-
-        Console.WriteLine($"Mi respuesta es: {respuesta}");
-
-        Console.WriteLine("¿Queres seguir preguntando?");
-
-        bool quiereSeguir = Console.ReadLine().ToLower().Equals("si");
-
-        if (!quiereSeguir) break;
     }
-    catch
-    {
-        Console.WriteLine("Ups. No pude contestar tu pregunta. Volve a preguntar \n");
 
+    private static bool QuiereSeguir()
+    {
+        Console.WriteLine("¿Queres seguir preguntando?");
+        return Console.ReadLine().ToLower().Equals("si");
     }
 }
