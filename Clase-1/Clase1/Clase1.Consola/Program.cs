0edd99a9a7f0053c3bc7d1627d31309df8485b85
nherreradev <nherrera3276@gmail.com>
2023-04-05 18:44:10 -0300

    TP |  COMMIT FALTANTE

--- Clase-1/Clase1/Clase1.Consola/Program.cs
+++ Clase-1/Clase1/Clase1.Consola/Program.cs
@@ -2,4 +2,25 @@
 
 Bola bola = new Bola();
 
-bola.Agitar();
+Boolean continue_ = true;
+
+while (continue_)
+{
+    Console.WriteLine("Ingrese una pregunta por si o por no, pulse 0 para salir");
+    string input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "":
+            break;
+        case "0":
+            Console.WriteLine("\nAdios.");
+            continue_ = false;
+            break;
+        default:
+            bola.Agitar();
+            break;
+    } 
+}
+    
+
