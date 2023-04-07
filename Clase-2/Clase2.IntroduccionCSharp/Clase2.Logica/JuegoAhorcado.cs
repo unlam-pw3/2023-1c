a1a29886defa012288abe282ae1a85e7a4fbe280
nherreradev <nherrera3276@gmail.com>
2023-04-07 16:06:02 -0300

    Clase2 | Agregado de nivel principiante y avanzado

--- Clase-2/Clase2.IntroduccionCSharp/Clase2.Logica/JuegoAhorcado.cs
+++ Clase-2/Clase2.IntroduccionCSharp/Clase2.Logica/JuegoAhorcado.cs
@@ -2,35 +2,72 @@
 {
     public class JuegoAhorcado
     {
-        private static List<string> PalabrasPosibles = new List<string>()
+        private static List<string> PalabrasNivelPrincipiante = new List<string>()
         {
             //Cosas que pueden estar en la cocina
-            "Cuchillo",
-            "Cuchara",
-            "Tenedor",
-            "Plato",
+            "Perro",
+            "Gato",
+            "Auto",
+            "Casa",
             "Vaso",
             "Mesa",
-            "Sarten",
-            "Cucharon",
-            "Mantel",
-            "Copa",
-            "Cafetera",
-            "Microondas",
-            "Refrigerador",
-            "Taza",
-            "Jarra",
+            "Pasto",
         };
-       
+
+        private static List<string> PalabrasNivelAvanzado = new List<string>()
+        {
+            //Cosas que pueden estar en la cocina
+            "Galaxya",
+            "Interprete",
+            "Introspeccion",
+            "Ambar",
+            "Jugueteria",
+            "Ambientalista",
+            "Xylofon",
+        };
+
+        private static List<string> listaActual = new List<string>();
+
         private List<string> LetrasAdivinadas = new List<string>();
         public void Ejecutar()
         {
             var dibujoAhorcado = new DibujoAhorcadoTradicional();
             var dibujadorPalabra = new DibujadorPalabra();
+            Console.WriteLine("Ingrese 1 para nivel principante | 2 para nivel avanzado | 0 Para cerrar");
+            string nivelIngresado = Console.ReadLine();
+            int nivelIngresadoInt = int.Parse(nivelIngresado);
+            bool continuar = true;
+
+            while (continuar)
+            {
+                if (nivelIngresadoInt == 1)
+                {
+                    listaActual = PalabrasNivelPrincipiante;
+                    continuar = false;
+                }
+                else if (nivelIngresadoInt == 2)
+                {
+                    listaActual = PalabrasNivelAvanzado;
+                    continuar = false;
+                }
+                else if (nivelIngresadoInt == 0)
+                {
+                    Console.WriteLine("Hasta la proxima");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese 1 para nivel principante | 2 para nivel avanzado | 0 Para cerrar");
+                    nivelIngresado = Console.ReadLine();
+                    nivelIngresadoInt = int.Parse(nivelIngresado);
+                }
+            }                     
+
+           
             //Crear lista de palabras posibles con alguna tematica
 
             //empezar juego y elegir una palabra
-            string palabraElegida = PalabrasPosibles[new Random().Next(PalabrasPosibles.Count())];
+            string palabraElegida = listaActual[new Random().Next(listaActual.Count())];
 
             //usuario ingresa una letra y se valida si pertenece a la palabra, si no pertenece se dibuja una parte del cuerpo del ahorcado
             do
@@ -56,7 +93,7 @@
                     //si no pertenece se dibuja una parte del cuerpo del ahorcado
                     //dibujar parte del cuerpo del ahorcado
                     dibujoAhorcado.DibujarAhorcado();
-                    dibujoAhorcado.RestarIntento();
+                    dibujoAhorcado.RestarIntento(nivelIngresadoInt);
                 }
 
             } while (dibujoAhorcado.QuedanIntentos());
