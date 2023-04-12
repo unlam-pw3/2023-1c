5fac24a71a064df1cd6de452d1585448d6cfc21f
atomasini <63592386+atomasini@users.noreply.github.com>
2023-04-12 19:56:51 -0300

    Juego ahorcado (#25)
    * Cambio en ChatGPTEjercicio de davinci a ChatGpt3_5Turbo por no soportado
    
    * Ejercicio Bola8Magica
    
    * nueva biblioteca OpenAI
    
    * agreado nuevos modo de juego
    
    * quitado de api key

---
+++ Clase-2/Clase2.IntroduccionCSharp/Clase2.Tests/ModoJuegoTest.cs
@@ -0,0 +1,36 @@
+﻿using Clase2.Logica;
+using Xunit;
+
+namespace Clase2.Tests
+{
+	public class ModoJuegoTest
+    {
+        [Fact]
+        public void ModoJuego_ObtenerPalabraModoDeJuegoPrincipiante()
+        {
+            // Arrange
+            var obtenerPalabra = new JuegoAhorcado();
+            int tamMinPalabra = 1;
+            int tamMaxPalabra = 6;
+            int modoJuegoPrincipiante = 1;
+            // Act
+            string palabraObtenida = obtenerPalabra.ObtenerPalabraSegunModoDeJuego(modoJuegoPrincipiante);
+            // Assert
+            Assert.InRange(palabraObtenida.Length, tamMinPalabra, tamMaxPalabra);
+        }
+
+        [Fact]
+        public void ModoJuego_ObtenerPalabraModoDeJuegoAvanzado()
+        {
+            // Arrange
+            var obtenerPalabra = new JuegoAhorcado();
+            int tamMinPalabra = 7;
+            int modoJuegoAvanzado = 2;
+            // Act
+            string palabraObtenida = obtenerPalabra.ObtenerPalabraSegunModoDeJuego(modoJuegoAvanzado);
+            // Assert
+            Assert.True(tamMinPalabra <= palabraObtenida.Length);
+        }
+
+    }
+}
