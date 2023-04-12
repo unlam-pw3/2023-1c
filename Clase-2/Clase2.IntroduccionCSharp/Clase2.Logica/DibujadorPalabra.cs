c4a921c91b6cb279064eb5db3c3508c333be62ea
Nicolas Kuhn <NKuhn@teco.com.ar>
2023-04-12 13:59:04 -0300

    resolucion

--- Clase-2/Clase2.IntroduccionCSharp/Clase2.Logica/DibujadorPalabra.cs
+++ Clase-2/Clase2.IntroduccionCSharp/Clase2.Logica/DibujadorPalabra.cs
@@ -1,35 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Clase2.Logica;
 
-namespace Clase2.Logica
+public class DibujadorPalabra
 {
-    public class DibujadorPalabra
+    private readonly IList<string> letrasAdivinadas;
+
+    public DibujadorPalabra(IList<string> letrasAdivinadas)
     {
-        public void DibujarPalabra(string palabraElegida, List<string> letrasAdivinadas)
-        {
-            string palabraADibujar = ObtenerDibujoPalabra(palabraElegida, letrasAdivinadas);
-            Console.WriteLine(palabraADibujar);
-        }
-
-        public string ObtenerDibujoPalabra(string palabraElegida, List<string> letrasAdivinadas)
-        {
-            string palabraADibujar = "";
-            foreach (char letra in palabraElegida)
-            {
-                if (letrasAdivinadas.Contains(letra.ToString(), StringComparer.OrdinalIgnoreCase))
-                {
-                    palabraADibujar += $"{letra} ";
-                }
-                else
-                {
-                    palabraADibujar += "_ ";
-                }
-            }
-            return palabraADibujar.Trim();
-        }
-
+        this.letrasAdivinadas = letrasAdivinadas;
     }
+
+    public void DibujarPalabra(string palabraElegida)
+    {
+        string palabraADibujar = ObtenerDibujoPalabra(palabraElegida);
+        Console.WriteLine(palabraADibujar);
+    }
+
+    public string ObtenerDibujoPalabra(string palabraElegida)
+    {
+        var palabraADibujar = palabraElegida.Select(letra => FueLetraAdivinada(letra) ? $"{letra} " : "_ ");
+
+        return string.Concat(palabraADibujar).Trim();
+    }
+
+    private bool FueLetraAdivinada(char letra)
+    {
+        return letrasAdivinadas.Contains(letra.ToString(), StringComparer.OrdinalIgnoreCase);
+    }
+
 }
---
+++ Clase-2/Clase2.IntroduccionCSharp/Clase2.Logica/DiccionarioPalabras.cs
@@ -0,0 +1,28 @@
+﻿namespace Clase2.Logica;
+
+public class DiccionarioPalabras
+{
+
+    public static readonly List<string> palabrasAvanzado = new List<string>()
+    {
+        "Cuchillo",
+        "Cuchara",
+        "Tenedor",
+        "Cucharon",
+        "Cafetera",
+        "Microondas",
+        "Refrigerador",
+    };
+
+    public static readonly List<string> palabrasPrincipiante = new List<string>()
+    {
+
+        "Poncho",
+        "Vaso",
+        "Mesa",
+        "Taza",
+        "Jarra",
+        "Mantel",
+        "Sarten",
+    };
+}
