5cd0f1b9f57738674ffbf7d3c3dbe040d834ba15
pablokuko <pablokuko@gmail.com>
2023-04-12 21:36:31 -0300

    Clase 3 mvc (#27)
    * agregando Ejemplo de MVC, mostrando un Listado de tipo de poderes
    
    * agregando logica de agregar tipo de poder y eliminar
    
    * agregando tarea

---
+++ Clase-3/Clase3.MVC/Clase3.MVC.Dominio/Entidades/Poder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.MVC.Dominio.Entidades
+{
+    public class Poder
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public TipoPoder Tipo { get; set; }
+        public int Daño { get; set; }
+    }
+}
