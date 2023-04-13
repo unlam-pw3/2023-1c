c392976f992fab4d0125c570d4ae645e4bd0e57e
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2023-04-12 21:08:20 -0300

    agregando Ejemplo de MVC, mostrando un Listado de tipo de poderes

---
+++ Clase-3/Clase3.MVC/Clase3.MVC.Dominio/Logica/TipoPoderRepositorio.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.MVC.Dominio.Logica
+{
+
+    public interface ITipoPoderRepositorio
+    {
+        List<Entidades.TipoPoder> ObtenerTodos();
+        void Agregar(Entidades.TipoPoder poder);
+        void Eliminar(int id);
+    }
+    public class TipoPoderRepositorio : ITipoPoderRepositorio
+    {
+        private static List<Entidades.TipoPoder> _tiposPoderes = new List<Entidades.TipoPoder>();
+
+        public TipoPoderRepositorio()
+        {
+            Inicializar();
+        }
+        private void Inicializar()
+        {
+            _tiposPoderes.Clear();
+            _tiposPoderes.Add(new Entidades.TipoPoder() { Id = 1, Nombre = "Ataque" });
+            _tiposPoderes.Add(new Entidades.TipoPoder() { Id = 2, Nombre = "Defensa" });
+            _tiposPoderes.Add(new Entidades.TipoPoder() { Id = 3, Nombre = "Curacion" });
+        }
+
+        public List<Entidades.TipoPoder> ObtenerTodos()
+        {
+            return _tiposPoderes;
+        }
+
+        public void Agregar(Entidades.TipoPoder tipoPoder)
+        {
+            _tiposPoderes.Add(tipoPoder);
+        }
+
+        public void Eliminar(int id)
+        {
+            var tipoPoder = _tiposPoderes.FirstOrDefault(x => x.Id == id);
+            if (tipoPoder != null)
+            {
+                _tiposPoderes.Remove(tipoPoder);
+            }
+        }
+    }
+}
---
+++ Clase-3/Clase3.MVC/Clase3.MVC.Web/Controllers/TipoPoderesController.cs
@@ -0,0 +1,19 @@
+﻿using Clase3.MVC.Dominio.Logica;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clase3.MVC.Web.Controllers
+{
+    public class TipoPoderesController : Controller
+    {
+        ITipoPoderRepositorio _tipoPoderRepositorio;
+        public TipoPoderesController(ITipoPoderRepositorio tipoPoderRepositorio)
+        {
+            _tipoPoderRepositorio = tipoPoderRepositorio;
+        }
+        public IActionResult Index()
+        {
+            var tiposPoderes = _tipoPoderRepositorio.ObtenerTodos();
+            return View(tiposPoderes);
+        }
+    }
+}
