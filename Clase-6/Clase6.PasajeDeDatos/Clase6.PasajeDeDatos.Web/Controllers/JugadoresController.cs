1660a781995ed7ce712aa2b5f32aad28b537b15d
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2023-05-03 19:46:57 -0300

    agregando ejemplo inicial de viewbag jugadores

---
+++ Clase-6/Clase6.PasajeDeDatos/Clase6.PasajeDeDatos.Web/Controllers/JugadoresController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clase6.PasajeDeDatos.Web.Controllers
+{
+    public class JugadoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<string> jugadores = new List<string>();
+            jugadores.Add("Leandro Paredes");
+            jugadores.Add("Julian Alvarez");
+      
+            ViewBag.Jugadores = jugadores;
+            return View();
+        }
+    }
+}
