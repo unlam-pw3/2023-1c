6aa0028169041d31f8ac0cb0f3a5ce1aa5db9a6d
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2023-05-03 20:00:25 -0300

    agregando ej viewbag tipado

--- Clase-6/Clase6.PasajeDeDatos/Clase6.PasajeDeDatos.Web/Controllers/JugadoresController.cs
+++ Clase-6/Clase6.PasajeDeDatos/Clase6.PasajeDeDatos.Web/Controllers/JugadoresController.cs
@@ -2,14 +2,36 @@
 
 namespace Clase6.PasajeDeDatos.Web.Controllers
 {
+    public class Jugador
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
     public class JugadoresController : Controller
     {
         public IActionResult Index()
         {
-            List<string> jugadores = new List<string>();
-            jugadores.Add("Leandro Paredes");
-            jugadores.Add("Julian Alvarez");
-      
+            List<string> competencias = new List<string>();
+            competencias.Add("Copa America Brasil 2021");
+            competencias.Add("Mundial Qatar 2022");
+            ViewBag.Competencias = competencias;
+
+            List<Jugador> jugadores = new List<Jugador>();
+            jugadores.Add(new Jugador()
+            {
+                Id = 1,
+                Nombre = "Julian",
+                Apellido = "Alvarez"
+            });
+
+            jugadores.Add(new Jugador()
+            {
+                Id = 2,
+                Nombre = "Leandro",
+                Apellido = "Paredes"
+            });
+
             ViewBag.Jugadores = jugadores;
             return View();
         }
