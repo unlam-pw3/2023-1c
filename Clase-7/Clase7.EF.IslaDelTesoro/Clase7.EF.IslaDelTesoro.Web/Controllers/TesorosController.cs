80dba0d95cd6145e9dd6b02d858528f26f417adf
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2023-05-24 20:58:56 -0300

    agregando logica para Editar Tesoro

--- Clase-7/Clase7.EF.IslaDelTesoro/Clase7.EF.IslaDelTesoro.Web/Controllers/TesorosController.cs
+++ Clase-7/Clase7.EF.IslaDelTesoro/Clase7.EF.IslaDelTesoro.Web/Controllers/TesorosController.cs
@@ -33,6 +33,53 @@ namespace Clase7.EF.IslaDelTesoro.Web.Controllers
             return View(tesoro);
         }
 
+        public IActionResult Editar(int id)
+        {
+            var tesoro = _context.Tesoros.Find(id);
+            if (tesoro != null)
+            {
+                return View(tesoro);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Editar2(Tesoro tesoro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Tesoros.Update(tesoro);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tesoro);
+        }
+        
+        [HttpPost]
+        public IActionResult Editar(Tesoro tesoro)
+        {
+            //Actualizando manualmente cada campo (es mas seguro)
+            if (ModelState.IsValid)
+            {
+                var tesoroEnBD = _context.Tesoros.Find(tesoro.Id);
+                if (tesoroEnBD == null)
+                {
+                    return View(tesoro);
+                }
+
+                tesoroEnBD.Nombre = tesoro.Nombre;
+                tesoroEnBD.Descripcion = tesoro.Descripcion;
+                tesoroEnBD.ImagenUrl = tesoro.ImagenUrl;
+                tesoroEnBD.Ubicacion = tesoro.Ubicacion;
+                tesoroEnBD.Valor = tesoro.Valor;
+
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tesoro);
+
+        }
+
         public IActionResult Eliminar(int id)
         {
             var tesoro = _context.Tesoros.Find(id);
