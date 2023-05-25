fee166adb20df1893be512f84b177f97d245951d
pablokuko <pablokuko@gmail.com>
2023-05-25 10:17:56 -0300

    Clase 7 ef isla del tesoro (#34)
    * commit inicial
    
    * agregando tabla Tesoro y EF para listar todos los tesoros
    
    * agregando logica para crear tesoro
    
    * agregando logica para eliminar tesoro
    
    * agregando logica para Editar Tesoro
    
    * agregando tarea (Agregar "Ubicacion")
    
    * fix readme

---
+++ Clase-7/Clase7.EF.IslaDelTesoro/Clase7.EF.IslaDelTesoro.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Clase7.EF.IslaDelTesoro.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clase7.EF.IslaDelTesoro.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
