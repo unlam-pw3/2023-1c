4287aaad489cf80dceb8e06cb90753a5247defef
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2023-06-07 20:19:49 -0300

    agregando categoria tesoro y pagina de lista

---
+++ Clase-7/Clase7.EF.IslaDelTesoro/Clase7.EF.IslaDelTesoro.Logica/CategoriaTesoroServicio.cs
@@ -0,0 +1,23 @@
+﻿using Clase7.EF.IslaDelTesoro.Data.Entidades;
+
+namespace Clase7.EF.IslaDelTesoro.Logica
+{
+    public interface ICategoriaTesoroServicio
+    {
+        List<CategoriaTesoro> ObtenerTodos();
+    }
+    
+    public class CategoriaTesoroServicio : ICategoriaTesoroServicio
+    {
+        private PW320231CEFIslaDelTesoroContext _contexto;
+        public CategoriaTesoroServicio(PW320231CEFIslaDelTesoroContext context)
+        {
+            _contexto = context;
+        }
+        public List<CategoriaTesoro> ObtenerTodos()
+        {
+            return _contexto.CategoriaTesoros.ToList();
+        }
+    }
+    
+}
---
+++ Clase-7/Clase7.EF.IslaDelTesoro/Clase7.EF.IslaDelTesoro.Web/Controllers/CategoriasTesorosController.cs
@@ -0,0 +1,18 @@
+﻿using Clase7.EF.IslaDelTesoro.Logica;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clase7.EF.IslaDelTesoro.Web.Controllers
+{
+    public class CategoriasTesorosController : Controller
+    {
+        private ICategoriaTesoroServicio _categoriaTesoroServicio;
+        public CategoriasTesorosController(ICategoriaTesoroServicio categoriaTesoroServicio)
+        {
+            _categoriaTesoroServicio = categoriaTesoroServicio;
+        }
+        public IActionResult Index()
+        {
+            return View(_categoriaTesoroServicio.ObtenerTodos());
+        }
+    }
+}
