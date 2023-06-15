84c03527a3e1343f56140f719a4bddb96e6652ff
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2023-06-14 21:23:32 -0300

    agregando logica para consumir desde un proyecto MVC, una api external (en este caso la hicimos nosotros). utilizando httpclient

---
+++ Clase-8/Clase8.Jugueteria/Clase8.Jugueteria.MVC/Models/HomeIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Clase8.Jugueteria.MVC.ApiClients;
+
+namespace Clase8.Jugueteria.MVC.Models
+{
+    public class HomeIndexViewModel
+    {
+        public List<JugueteApiModel> Juguetes { get; set; }
+    }
+}
