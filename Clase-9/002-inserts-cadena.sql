0706758cec68ba8c7e79f0536a37fb9203bf3c01
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2023-06-21 19:34:05 -0300

    agregando scripts de db

---
+++ Clase-9/002-inserts-cadena.sql
@@ -0,0 +1,10 @@
+USE [PW3-2023-1c-Modelo-2do-parcial]
+GO
+SET IDENTITY_INSERT [dbo].[Cadena] ON 
+GO
+INSERT [dbo].[Cadena] ([Id], [Razon_social]) VALUES (1, N'Coto')
+GO
+INSERT [dbo].[Cadena] ([Id], [Razon_social]) VALUES (2, N'Makro')
+GO
+SET IDENTITY_INSERT [dbo].[Cadena] OFF
+GO
