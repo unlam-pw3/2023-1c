2376f98b6def1eb39054be560fa3c402e0a381a8
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2023-06-21 19:50:14 -0300

    agregando modelo de clases de EF

---
+++ Clase-9/Clase9.Modelo2doParcial/SistemaDeCadenasAlimenticias.Data/EF/Cadena.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaDeCadenasAlimenticias.Data.EF;
+
+public partial class Cadena
+{
+    public int Id { get; set; }
+
+    public string RazonSocial { get; set; } = null!;
+
+    public virtual ICollection<Sucursal> Sucursals { get; set; } = new List<Sucursal>();
+}
