11f1ea004d3f21a18abbceaeb2382534be2302f5
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2023-06-21 19:19:44 -0300

    subiendo solucion y proyecto mvc por defecto

---
+++ Clase-9/Clase9.Modelo2doParcial/Clase9.Modelo2doParcial.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Clase9.Modelo2doParcial.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
